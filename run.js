3506ebd824c9fdf37cb74f4b382a3e566839b034
Jonathan Kim <jkimbo@gmail.com>
2011-12-07 15:14:29 +0000

    Some clean up and documentation

--- run.js
+++ run.js
@@ -1,12 +1,12 @@
 /*
  * Run fizzbuzz
  */
-var fizzbuzz = require('./lib/fizzbuzz.js');
+var fizzbuzz = require('./lib/fizzbuzz.js'); // load in the fizzbuzz module
 
 for(var i = 1; i <= 100; i++) {
     var output = fizzbuzz.check(i);
-    if(!output) {
-        output = i;
+    if(!output) { // if no result from fizzbuzz
+        output = i; // output current number
     }
     console.log(output);
 }
--- test/test-fizzbuzz.js
+++ test/test-fizzbuzz.js
@@ -1,10 +1,17 @@
-var fizzbuzz = require('../lib/fizzbuzz.js');
+/*
+ * FizzBuzz tests
+ */
 
+var fizzbuzz = require('../lib/fizzbuzz.js'); // load in fizzbuzz module
+
+/* Basic tests */
 exports['basic-inputs'] = function(test) {
     test.equal(fizzbuzz.check(3), 'Fizz'); // test multiple of 3
     test.equal(fizzbuzz.check(5), 'Buzz'); // test multiple of 5
     test.equal(fizzbuzz.check(15), 'FizzBuzz'); // test multiple of 3 and 5
     test.equal(fizzbuzz.check(0), ''); // test 0 case
+
+    /* Various edge cases */
     test.throws(function() { fizzbuzz.check() });
     test.throws(function() { fizzbuzz.check(null) });
     test.throws(function() { fizzbuzz.check(true) });
@@ -13,14 +20,16 @@ exports['basic-inputs'] = function(test) {
     test.throws(function() { fizzbuzz.check('asfl') });
     test.throws(function() { fizzbuzz.check('3') });
     test.throws(function() { fizzbuzz.check('1234') });
+
     test.done();
 }
 
+/* Random number tests */
 exports['random-inputs'] = function(test) {
     // give random number multiplied by 3
     test.expect(30);
     for(var i = 1; i <= 10; i++) {
-        var rand = Math.floor(Math.random()*10)*3;
+        var rand = Math.floor(Math.random()*100)*3;
         var output = fizzbuzz.check(rand);
         if(output == 'Fizz' || output == 'FizzBuzz' || rand == 0) {
             test.ok(true);
@@ -31,7 +40,7 @@ exports['random-inputs'] = function(test) {
     
     // random number multiplied by 5
     for(var i = 1; i <= 10; i++) {
-        var rand = Math.floor(Math.random()*10)*5;
+        var rand = Math.floor(Math.random()*100)*5;
         var output = fizzbuzz.check(rand);
         if(output == 'Buzz' || output == 'FizzBuzz' || rand == 0) {
             test.ok(true);
@@ -42,7 +51,7 @@ exports['random-inputs'] = function(test) {
 
     // random number multiplied by 15
     for(var i = 1; i <= 10; i++) {
-        var rand = Math.floor(Math.random()*10)*15;
+        var rand = Math.floor(Math.random()*100)*15;
         var output = fizzbuzz.check(rand);
         if(output == 'FizzBuzz' || rand == 0) {
             test.ok(true);
